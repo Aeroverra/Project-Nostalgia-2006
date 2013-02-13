2171f96d01524ed295e73afb099d363a5449a94e
nostalgia2006 <runescape06com@gmail.com>
2013-02-13 22:33:05 +0100

    Update src/server/game/players/packets/IdleLogout.java
    Cleaner

--- src/server/game/players/packets/IdleLogout.java
+++ src/server/game/players/packets/IdleLogout.java
@@ -9,9 +9,7 @@ public class IdleLogout implements PacketType {
 
 	@Override
 	public void processPacket(Client c, int packetType, int packetSize) {
-		if (c.playerRights >= 2) {
-		    //do nothing hahahah
-		} else {
+		if (c.playerRights < 2) {
 		c.logout();
 	    //TODO make a idle timer
 		}
